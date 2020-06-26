15bff5a910b826c9a7b7ea39ade7239f7bd7ca0a
ferhatu <ferhat_2001@yahoo.com>
2020-06-26 14:43:12 +0100

    update

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/naber", (req, res) => {
+  res.send("hello me nnnnnn");
 });
 
 const myPort = 5000;
